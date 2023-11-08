4fba6bf918cadd2446c4e94a1eb47d553f89ec18
Atef Moazzen <68507159+atef1995@users.noreply.github.com>
2023-11-08 17:02:03 +0100

    modified:   package-lock.json 	modified:   package.json 	deleted:    src/App.css 	modified:   src/App.js 	deleted:    src/App.test.js 	new file:   src/components/CarForm.js 	new file:   src/components/CarList.js 	new file:   src/components/CarSearch.js 	new file:   src/components/CarValue.js 	deleted:    src/index.css 	modified:   src/index.js 	deleted:    src/logo.svg 	deleted:    src/reportWebVitals.js 	deleted:    src/setupTests.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import CarList from "./components/CarList"
+import CarForm from "./components/CarForm"
+import CarSearch from "./components/CarSearch"
+import CarValue from "./components/CarValue"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+export default function App() {
+    return (
+        <>
+            <CarForm />
+            <CarList />
+            <CarSearch />
+            <CarValue />
+        </>
+
+    )
+} 

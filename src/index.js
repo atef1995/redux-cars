42b9fb315d8e7c0fa63b1d0daf25d5822199ae71
Atef Moazzen <68507159+atef1995@users.noreply.github.com>
2023-11-12 21:41:49 +0100

    modified:   src/App.js 	modified:   src/components/CarForm.js 	modified:   src/components/CarList.js 	modified:   src/components/CarSearch.js 	modified:   src/components/CarValue.js 	modified:   src/index.js 	new file:   src/store/index.js 	new file:   src/store/slices/carsSlice.js 	new file:   src/store/slices/formSlice.js 	new file:   src/styles.css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
+import 'bulma/css/bulma.css';
+import './styles.css'
 import React from "react";
 import { createRoot } from 'react-dom/client';
 import App from './App'
+import { store } from "./store";
+import { Provider } from "react-redux";
 
 
 const root = createRoot(document.getElementById('root'));
-root.render(<App />)
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+)
---
+++ src/store/slices/carsSlice.js
@@ -0,0 +1,24 @@
+import { createSlice, nanoid } from '@reduxjs/toolkit'
+
+const carsSlice = createSlice({
+    name: 'cars',
+    initialState: {
+        searchTerm: '',
+        data: [],
+    },
+    reducers: {
+        changeSearchTerm(state, action) {
+            state.searchTerm = action.payload;
+        },
+        addCar(state, action) {
+            state.data.push({ name: action.payload.name, cost: action.payload.cost, id: nanoid() });
+        },
+        removeCar(state, action) {
+            const updatedCar = state.data.filter((car) => { return car.id !== action.payload });
+            state.data = updatedCar;
+        }
+    }
+});
+
+export const { changeSearchTerm, addCar, removeCar } = carsSlice.actions;
+export const carsReducer = carsSlice.reducer;
